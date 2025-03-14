8700945d9812cf212fecf1583a87c9a206110c62
KornOse <kornel.oselsky9@gmail.com>
2025-03-14 15:43:16 +0100

    test2

--- tasks.py
+++ tasks.py
@@ -5,11 +5,11 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-template_loader = jinja2.FileSystemLoader("templates")
-template_env = jinja2.Environment(loader=template_loader)
+# template_loader = jinja2.FileSystemLoader("templates")
+# template_env = jinja2.Environment(loader=template_loader)
 
-def render_template(template_filename, **context):
-  return template_env.get_template(template_filename).render(**context)
+# def render_template(template_filename, **context):
+#   return template_env.get_template(template_filename).render(**context)
 
 def send_simple_message(to, subject, body, html):
   domain = os.getenv("MAILGUN_DOMAIN")
